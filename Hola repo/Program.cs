1cfafaa8819f60343840255fbe12eab2221b8b8e
Raul Aldriguetti Bolanios <00300075@red.unid.mx>
2020-11-26 06:43:23 -0600

    version 1.2

--- Hola repo/Program.cs
+++ Hola repo/Program.cs
@@ -10,13 +10,33 @@ namespace Hola_repo
     {
         static void Main(string[] args)
         {
+
+            int numero1, numero2, resultado;
             Console.WriteLine("hola mundo");
-
-
-            Console.WriteLine("hola amigos");
-
-
+            Console.WriteLine("hola amigos"); 
             Console.WriteLine("como estan");
+            //codigo que suma dos numeros 
+            Console.WriteLine("suma de dos numeros");
+            Console.WriteLine("primer numero:");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("segundo muero:");
+            numero2 = int.Parse(Console.ReadLine());
+           
+            
+           
+            resultado = numero1 + numero2;
+            Console.WriteLine("el resultado es  " + resultado);
+
+
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
